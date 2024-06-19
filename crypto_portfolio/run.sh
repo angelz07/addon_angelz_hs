db5ca3b70417e4eebd5fd87377ebd2c915ab2781
angelz07 <fabrice@telecom4all.be>
2024-06-19 14:21:27 +0200

    Update run.sh

--- crypto_portfolio/run.sh
+++ crypto_portfolio/run.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/env bashio
+#!/bin/bash
 
+# Activer l'environnement virtuel si nécessaire (à ajouter si utilisé)
+# source /path/to/venv/bin/activate
+
+# Exécuter le script Python
 python3 /app/crypto_portfolio.py
-
